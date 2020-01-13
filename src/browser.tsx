9e9bb2acb495bdb4a9eade97b572232c302a0966
Brian LeRoux <b@brian.io>
2020-01-12 19:38:44 -0800

    make this easier

--- src/browser.tsx
+++ src/browser.tsx
@@ -1,8 +1,7 @@
 import { React, ReactDOM } from 'https://unpkg.com/es-react@16.8.60/index.js'
-import { Clock } from './components/clock.tsx'
+import { Clock } from './http/get-index/clock.tsx'
 
 window.addEventListener("DOMContentLoaded", function(event) {
-  // @ts-ignore
   let el = window.document.getElementById('root')
   ReactDOM.hydrate(<Clock />, el)
 })
